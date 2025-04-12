f4aa8223f67005920c54246765ec232bdf00e5d4
Accord <dawid.zgoda@edu.wsti.pl>
2025-04-12 15:01:16 +0200

    Remake

--- Zadanie11.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-public class Zadanie11 {
-    public static void main(String[] args) {
-        String text = JOptionPane.showInputDialog("Wpisz tekst:");
-        text = text.toUpperCase();
-        JOptionPane.showMessageDialog(null, text);
-        System.exit(1);
-    }
-}
