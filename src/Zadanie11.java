c9faeb0f01c10b8756664731c5b52d188c1d90a5
Accord <dawid.zgoda@edu.wsti.pl>
2025-04-12 14:52:22 +0200

    Update Zadanie11.java

--- src/Zadanie11.java
+++ src/Zadanie11.java
@@ -1,8 +1,7 @@
 import javax.swing.*;
 public class Zadanie11 {
     public static void main(String[] args) {
-        JOptionPane input = new JOptionPane();
-        String text = JOptionPane.showInputDialog(input);
+        String text = JOptionPane.showInputDialog("Wpisz tekst:");
         text = text.toUpperCase();
         JOptionPane.showMessageDialog(null, text);
         System.exit(1);
